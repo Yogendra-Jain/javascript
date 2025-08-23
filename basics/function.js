19b26390b7e6f30ac93e8fb8f8898c851a7bb0c1
Yogendra-Jain <165588778+Yogendra-Jain@users.noreply.github.com>
2025-08-23 01:52:07 +0000

    function

---
+++ basics/function.js
@@ -0,0 +1,21 @@
+const arrow = ()=>{
+    let username = "hitesh";
+    // console.log(this.username); //undefined
+    console.log(username);
+}
+
+
+arrow();
+
+const addTow = (a,b)=>{
+    console.log(a+b)
+}
+
+addTow(2,3)
+
+const add = (a,b)=>{
+    return a+b;
+}
+
+console.log(add(4,5));
+
