9489557659370989d20fc50f4b83e05b53cd8453
Yogendra-Jain <165588778+Yogendra-Jain@users.noreply.github.com>
2025-08-26 13:51:22 +0000

    reduce complete

--- control_flow/reduce.js
+++ control_flow/reduce.js
@@ -10,3 +10,26 @@ console.log(myTotal);
 let Total = myNum.reduce((accumulator,item)=> accumulator + item,0);
 
 console.log(Total);
+
+let shoppingCart =[
+    {
+        item :'js course',
+        price : 999
+    },
+    {
+        item :'py course',
+        price : 1999
+    },
+    {
+        item :'cpp course',
+        price : 2999
+    },
+    {
+        item :'java course',
+        price : 5999
+    }
+]
+
+let totalprice = shoppingCart.reduce((acc,item)=> acc + item.price ,0);
+
+console.log(totalprice);
